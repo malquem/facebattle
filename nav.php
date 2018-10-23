03af64f0d0871ad9d30867a2f97838e60b9beaf0
malquem <um-id@ya.ru>
2018-10-23 17:23:08 +0300

    push+

--- nav.php
+++ nav.php
@@ -1,8 +1,7 @@
-<section>
+
 <ul>
     <li class="current" style="white-space: nowrap;"><a href="index.php"> Главная</a> </li>
     <li style="white-space: nowrap;"><a href="#"> Битва</a> </li>
     <li style="white-space: nowrap;"><a href="#"> ТОП10</a> </li>
     <li style="white-space: nowrap;"><a href="#"> Авторизация</a> </li>
 </ul>
-</section>
