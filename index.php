c3dc4948ccdd1aba5e8f75f25c8d8c2bf965a42b
malquem <um-id@ya.ru>
2018-10-23 16:49:02 +0300

    push

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 include 'header.php';
 ?>
+<section id="header">
+    <h1>Добро пожаловать в FaceBattle!</h1>
+    <nav id="nav">
+        <?php include 'nav.php' ?>
+    </nav>
+</section>
+
+<section id="intro">
+    <h2>Голосуй за фейс и получи ценный кейс!</h2>
+</section>
---
+++ nav.php
@@ -0,0 +1,6 @@
+<ul>
+    <li class="current" style="white-space: nowrap;"><a href="index.php"> Главная</a> </li>
+    <li style="white-space: nowrap;"><a href="#"> Битва</a> </li>
+    <li style="white-space: nowrap;"><a href="#"> ТОП10</a> </li>
+    <li style="white-space: nowrap;"><a href="#"> Авторизация</a> </li>
+</ul>
