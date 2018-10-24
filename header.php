0374b3b1a9479ffef2387448996707b80a9bdc36
malquem <um-id@ya.ru>
2018-10-24 17:11:06 +0300

    push+

--- header.php
+++ header.php
@@ -1,4 +1,13 @@
-<link href="main.css" rel="stylesheet">
+<!DOCTYPE html>
+<html>
+<head>
+	<title>FaceBattle</title>
+	<meta charset="utf-8">
+	<link href="main.css" rel="stylesheet">
+</head>
+
+
+
 <?php
 /** * Date: 16.10.2018 * Time: 12:13 */
 echo 'header included!';
--- index.php
+++ index.php
@@ -14,26 +14,27 @@ include 'header.php';
 			<section class="first">
 				<i class="icon featured fa-cog"></i>
 				<header>
-					<h2>Ipsum consequat</h2>
+					<h2>Гюльчатай</h2>
 				</header>
-				<p>Nisl amet dolor sit ipsum veroeros sed blandit consequat veroeros et magna tempus.</p>
+				<p><img src="img/like.png"> <img src="img/dislike.png"></p>
 			</section>
 		</div>
 		<div class="col-6 col-12-medium">
 			<section class="middle">
 				<i class="icon featured alt fa-flash"></i>
 				<header>
-					<h2>Magna etiam dolor</h2>
+					<h2>Шакира</h2>
 				</header>
-				<p>Nisl amet dolor sit ipsum veroeros sed blandit consequat veroeros et magna tempus.</p>
+				<p><img src="img/like.png"> <img src="img/dislike.png"></p>
+
 			</section>
 		</div>
 
 	</div>
 	<footer>
 		<ul class="actions">
-			<li><a href="#" class="button large">Get Started</a></li>
-			<li><a href="#" class="button alt large">Learn More</a></li>
+			<li><a href="#" class="button large">Дальше</a></li>
+
 		</ul>
 	</footer>
 </section>
