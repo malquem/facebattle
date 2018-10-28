b188366270259dc2da356e11fde3b5a49ce9b442
malquem <um-id@ya.ru>
2018-10-28 20:45:51 +0300

    re

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+
+
+define ("DB_USER", "root");
+define ("DB_PASSWORD", "");
+define ("DB_DATABASE", "facebattle");
+define ("DB_HOST", "localhost");
+
+
+$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+?>
